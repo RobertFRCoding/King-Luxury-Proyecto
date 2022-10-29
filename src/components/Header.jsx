68a3c1560ca80b7f36a5754208ebea86c7300978
Catarina <catacoding@gmail.com>
2022-10-29 23:39:38 +0100

    modified:   index.html 	modified:   src/components/Carrusel.jsx 	modified:   src/components/Header.jsx 	deleted:    src/images/Eye.png 	new file:   src/images/breakfast-service.jpeg 	deleted:    src/images/ksl-no-background.svg 	new file:   src/images/lambo-white .jpeg 	renamed:    src/images/Logo No Background.svg -> src/images/logos/kls-no-background.svg 	renamed:    src/images/metamask-icon.svg -> src/images/logos/metamask-icon.svg 	new file:   src/images/mercedes-parked.jpeg 	deleted:    src/images/metamask-logo.png 	deleted:    src/images/metamask-logo.svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,10 @@ import { NavLink } from "react-router-dom"
 import { useState } from "react"
 
 export function Header () {
-    return         <div className="m-1 text-dark d-flex justify-content-between"> 
-        <img src="/src/images/ksl-no-background.svg" alt="" width="105" height="80" className="d-inline-block align-text-top"/>
+    return  <div className="m-1 text-dark d-flex justify-content-between"> 
         <nav class="navbar navbar-expand-lg navbar-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="#"><img src="/src/images/logos/kls-no-background.svg" alt="" width="105" height="80" className="d-inline-block align-text-top"/></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -29,13 +28,24 @@ export function Header () {
             <li><a class="dropdown-item" href="#">Cars</a></li>
             <li><a class="dropdown-item" href="#">Villas</a></li>
             <li><a class="dropdown-item" href="#">Yatchs</a></li>
+            <li><a class="dropdown-item" href="#">Jets</a></li>
+            <li><a class="dropdown-item" href="#">Transfers</a></li>
+            <li><a class="dropdown-item" href="#">Catering</a></li>
           </ul>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-        <img src="/src/images/metamask-icon.svg" alt="" width="50" height="34" className="d-inline-block align-text-top"/>
+<nav class="navbar navbar-light">
+  <div class="container-fluid">
+    <form class="d-flex">
+      <input class="form-control me-1" type="search" placeholder="Search Me" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<img src="/src/images/logos/metamask-icon.svg" alt="" width="50" height="34" className="d-inline-block align-text-top"/>
     </div>
 
 }  
